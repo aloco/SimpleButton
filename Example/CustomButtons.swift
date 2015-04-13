120d5fa553eb68c2e82e5eba1f56c76e01ef7d80
Andreas Tinoco Lobo <andreas@tinoco-lobo.at>
2015-04-13 11:08:22 +0200

    extended examples

---
+++ Example/CustomButtons.swift
@@ -0,0 +1,76 @@
+//
+//  CustomButtons.swift
+//  Example
+//
+//  Created by Andreas Tinoco Lobo on 08.04.15.
+//  Copyright (c) 2015 Andreas Tinoco Lobo. All rights reserved.
+//
+
+import Foundation
+import SimpleButton
+
+class ScaleButton: SimpleButton {
+    
+    override func configurate() {
+        super.configurate()
+        setBackgroundColor(UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0), forState: .Normal)
+        setTitle(".Normal", forState: .Normal)
+        setTitle(".Highlighted", forState: .Highlighted)
+        setScale(0.97, forState: .Highlighted)
+        
+    }
+}
+
+class BackgroundColorButton: SimpleButton {
+    
+    override func configurate() {
+        super.configurate()
+        setBackgroundColor(UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1.0), forState: .Normal)
+        setBackgroundColor(UIColor(red: 211/255, green: 84/255, blue: 0/255, alpha: 1.0), forState: .Highlighted)
+        setTitle(".Normal", forState: .Normal)
+        setTitle(".Highlighted", forState: .Highlighted)
+        
+    }
+}
+
+class BorderWidthButton: SimpleButton {
+    
+    override func configurate() {
+        super.configurate()
+        setTitleColor(UIColor(red: 155/255, green: 89/255, blue: 182/255, alpha: 1.0), forState: .Normal)
+        setBorderColor(UIColor(red: 155/255, green: 89/255, blue: 182/255, alpha: 1.0), forState: .Normal)
+        setTitle(".Normal", forState: .Normal)
+        setTitle(".Highlighted", forState: .Highlighted)
+        setBorderWidth(2.0, forState: .Normal)
+        setBorderWidth(8.0, forState: .Highlighted)
+        
+    }
+}
+
+class BorderColorButton: SimpleButton {
+    
+    override func configurate() {
+        super.configurate()
+        setTitleColor(UIColor.grayColor(), forState: .Normal)
+        setBorderWidth(4.0, forState: .Normal)
+        setBorderColor(UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0), forState: .Normal)
+        setBorderColor(UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0), forState: .Highlighted)
+        setTitle(".Normal", forState: .Normal)
+        setTitle(".Highlighted", forState: .Highlighted)
+        
+    }
+}
+
+class CornerRadiusButton: SimpleButton {
+    
+    override func configurate() {
+        super.configurate()
+        
+        setBackgroundColor(UIColor(red: 26/255, green: 188/255, blue: 156/255, alpha: 1.0), forState: .Normal)
+        setTitle(".Normal", forState: .Normal)
+        setTitle(".Highlighted", forState: .Highlighted)
+        setCornerRadius(10.0, forState: .Normal)
+        setCornerRadius(20.0, forState: .Highlighted)
+
+    }
+}
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -11,42 +11,23 @@ import SimpleButton
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var borderButton: UpperCaseButton!
-    @IBOutlet weak var disabledButton: SimpleButton!
-    @IBOutlet weak var scaledButton: SimpleButton!
-    @IBOutlet weak var primaryButton: PrimaryButton!
-    
-    var simpleButton: SimpleButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        let awesomeButton = SimpleButton.buttonWithType(.Custom) as! SimpleButton
+        awesomeButton.setTitle("Awesome".uppercaseString, forState: .Normal)
+        awesomeButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        awesomeButton.setBorderWidth(2.0, forState: .Normal)
+        awesomeButton.setBorderColor(UIColor.blackColor(), forState: .Normal)
+        awesomeButton.setCornerRadius(65, forState: .Normal)
+        awesomeButton.setScale(0.95, forState: .Highlighted)
         
-        primaryButton.setTitle("Make your own subclass", forState: UIControlState.Normal)
+        awesomeButton.setNeedsLayout()
+        awesomeButton.frame = CGRectMake(0, 0, 130, 130)
+        awesomeButton.center = CGPointMake(view.center.x, view.frame.size.height - 90)
+        awesomeButton.setTranslatesAutoresizingMaskIntoConstraints(true)
+        view.addSubview(awesomeButton)
         
-        borderButton.setBorderWidth(4.0, forState: .Normal)
-        borderButton.setBorderWidth(2.0, forState: .Highlighted)
-        borderButton.setCornerRadius(10, forState: .Normal)
-        borderButton.setCornerRadius(20, forState: .Highlighted)
-
-        borderButton.setTitle("borderColor", forState: UIControlState.Normal)
-        borderButton.setBorderColor(UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1.0), forState: UIControlState.Normal)
-        borderButton.setBorderColor(UIColor(red: 149/255, green: 165/255, blue: 166/255, alpha: 1.0), forState: UIControlState.Highlighted)
-        borderButton.setTitleColor(UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1.0), forState: UIControlState.Normal)
-        
-        scaledButton.layer.cornerRadius = 50
-        scaledButton.titleLabel?.numberOfLines = 0
-        scaledButton.titleLabel?.textAlignment = .Center
-        scaledButton.setTitle("scale", forState: UIControlState.Normal)
-        scaledButton.setBackgroundColor(UIColor(red: 26/255, green: 188/255, blue: 156/255, alpha: 1.0), forState: UIControlState.Normal)
-        scaledButton.setBackgroundColor(UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0), forState: UIControlState.Highlighted)
-        scaledButton.setScale(0.95, forState: UIControlState.Highlighted)
-        
-        disabledButton.setTitle("disabled Button", forState: UIControlState.Normal)
-        disabledButton.setTitleColor(UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0), forState: UIControlState.Normal)
-        disabledButton.setTitleColor(UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1.0), forState: UIControlState.Highlighted)
-        disabledButton.setBackgroundColor(UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0), forState: UIControlState.Normal)
-        disabledButton.setBackgroundColor(UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0), forState: UIControlState.Highlighted)
-        disabledButton.enabled = false
     }
 }
-
